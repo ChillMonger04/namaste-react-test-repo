323f69f4b8464134252662848bead9150bee8c93
Jasjeet <jasjeet_mann@yahoo.com>
2024-07-15 23:56:30 +0530

    Learned about Components

--- script.js
+++ script.js
@@ -69,3 +69,42 @@ const parent = React.createElement("div", { id: "parent" }, [
 // NOTE -> IF ANYTHING WAS ALREADY THERE IS MAIN DIV IT WILL GET REPLACED WITH THE NEW HEADING WE CREATED
 // NOTE -> BUT IF WE HAVE ELEMENTS ABOVE/BELOW THE MAIN WE ARE RENDERING IT WILL NOT BE REPLACED
 main.render(parent);
+
+// JSX -> Similar to HTML or XML syntax
+// This JSX code is not accepted by React or is not Javascript code,
+// But we can still see the output of this JSX code on our screen
+// This is because this JSX code is TRANSPILED by Parcel with the help of Babel
+// Babel is a JavaScript transpiler that is mainly used to convert ECMAScript code into backwards-compatible JavaScript code
+// JSX is transpiled into React.createElement before getting rendered. After that is is rendered as React.createElement
+// Babel converts JSX Code to React.createElement code
+// Round brackets in jsx for multi line code
+// Camel casing is used in JSX Code. Use of className instead of class.
+const jsxHeading = <h1 className="jsxHeading">Hello World from JSX</h1>;
+
+// Components in React JS
+// There are 2 types of components in React
+// 1. Functional Components (NEW METHOD) - They are simple JavaScript functions that return JSX
+// 2. Class Components (OLD METHOD) - They are JavaScript classes that extend the React.Component class
+
+// The component name should always be CAPITALIZED
+// Component Name can be created using arrow function and regular function also
+const TileComponent = function () {
+  return <div className="tileComponent">Hello World from Tile Component</div>;
+};
+
+// Used fragments as we can return only one thing at a time
+// Component Composition -> Component inside a component
+const HeadingComponent = () => {
+  return (
+    <>
+      <TileComponent />
+      <h1 className="headingComponent">Hello World from Heading Component</h1>
+    </>
+  );
+};
+
+// Method to render jsx element
+main.render(jsxHeading);
+
+// Method to render functional component
+main.render(<HeadingComponent />);
